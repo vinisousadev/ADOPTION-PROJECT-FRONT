08e5d5cedf1230414bf3fab1906acaf1c0a12400
Vinicius Sousa <marcusviniciusformula@gmail.com>
2026-05-06 13:02:42 -0300

    Fixed unexpected token

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { getStoredToken } from '../utils/authStorage'
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL ?? import.meta.env.VITE_API_BASE_URL,
 })
 
 api.interceptors.request.use((config) => {
